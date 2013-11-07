adca6fb4109773ef6791c06a4535e49c4de57033
LinuxMercedes <LinuxMercedes@gmail.com>
2013-11-07 14:45:00 -0600

    Test view for model generation

--- AgoraD/loading_dock/urls.py
+++ AgoraD/loading_dock/urls.py
@@ -4,5 +4,6 @@ from loading_dock import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
+    url(r'^(?P<database>\w+)/(?P<table>\w+)/?$', views.dblist, name='dblist'),
     )
 
--- AgoraD/loading_dock/views.py
+++ AgoraD/loading_dock/views.py
@@ -1,1 +1,22 @@
-# Create your views here.
+from django.http import HttpResponse
+import ModelGenerator
+from ModelGenerator import data
+from ModelGenerator.data import newtable
+
+def index(request):
+  return HttpResponse("hi")
+
+def dblist(request, *args, **kwargs):
+  print ModelGenerator
+  t = ModelGenerator.getModel(kwargs['database'], kwargs['table'])
+
+  print ModelGenerator.data
+
+  print t
+  s = ''
+  for i in t.objects.all():
+    s += str((i.c1, i.c2))
+
+  return HttpResponse(s)
+
+
