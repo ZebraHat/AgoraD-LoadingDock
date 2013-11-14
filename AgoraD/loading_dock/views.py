b589ab07ea8b829f8380e405587829a72283d00c
LinuxMercedes <LinuxMercedes@gmail.com>
2013-11-14 15:18:29 -0600

    Test json serializer

--- AgoraD/loading_dock/views.py
+++ AgoraD/loading_dock/views.py
@@ -1,32 +1,20 @@
 from django.http import HttpResponse
-import ModelGenerator
-from ModelGenerator import data
-from ModelGenerator.data import newtable
+from django.core import serializers
+from ModelGenerator import getModel
 
 def index(request):
   return HttpResponse("hi")
 
 def dblist(request, *args, **kwargs):
-  print ModelGenerator
-  t = ModelGenerator.getModel(kwargs['database'], kwargs['table'])
+  t = getModel(kwargs['database'], kwargs['table'])
 
-  print ModelGenerator.data
+  #v = t(c1 = 5, c2 = "asdf")
+  #v.save()
 
-  print t
+  response = HttpResponse()
 
-  v = t(c1 = 5, c2 = "asdf")
-  v.save()
+  serializers.serialize('json', t.objects.all(), stream=response)
 
-  s = ''
-  for i in t.objects.all():
-    s += str((i.c1, i.c2))
-
-  s += "\n"
-
-  for i in t.__fields__:
-    s += str(i)
-    s += "\n"
-
-  return HttpResponse(s)
+  return response
 
 
