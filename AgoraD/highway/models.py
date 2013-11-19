0cdb7179d5e3a8a3e366c1ec0953f2b32b84c47d
Chase Holland <chase.holland@lumate.com>
2013-11-19 17:31:05 -0600

    adding block creation code

--- AgoraD/highway/models.py
+++ AgoraD/highway/models.py
@@ -5,10 +5,31 @@
 
 __author__ = 'chase'
 
-from django.db.models import Model
+from django.db import models
+from AgoraD.loading_dock.models import Database, Table
 
 
-class Session(Model):
+class Session(models.Model):
+    def __unicode__(self):
+        return str(self.session_id)
+
     #TODO Layout session
     # logger is default database
-    pass
+    session_id = models.CharField(max_length=256, help_text="Session ID from Marketplace")
+
+    class Meta:
+        app_label = 'highway'
+
+
+class Block(models.Model):
+    def __unicode__(self):
+        return '{}: {}: {} - {}'.format(self.database, self.table, self.start_row, self.end_row)
+
+    database = models.ForeignKey(Database)
+    table = models.ForeignKey(Table)
+
+    start_row = models.IntegerField()
+    end_row = models.IntegerField()
+
+    class Meta:
+        app_label = 'highway'
--- AgoraD/highway/modules/blocks.py
+++ AgoraD/highway/modules/blocks.py
@@ -6,7 +6,8 @@ __author__ = 'chase'
 
 
 import loading_dock.ModelGenerator
-from loading_dock.models import Database, Tables, Column
+from loading_dock.models import Database, Table, Column
+from highway.models import Block
 
 
 def create_blocks():
@@ -16,7 +17,27 @@ def create_blocks():
     # create block object
 
     ## grab a list of all databases, only support first for now
+
+    block_size = 1000
+
     database = Database.objects.all()[0]
-    tables = Tables.objects.get(db=database)
+    tables = Table.objects.filter(db=database)
     for table in tables:
+        num_rows = table.objects.count()
+        for x in range(0, num_rows, block_size):
+            Block.objects.create(
+                database=database,
+                table=table,
+                start_row=x,
+                end_row=x + block_size - 1
+            )
+        ## do the last few blocks
+        if num_rows % block_size:
+            Block.objects.create(
+                database=database,
+                table=table,
+                start_row=num_rows - num_rows % block_size,
+                end_row=num_rows - 1
+            )
+
         pass
