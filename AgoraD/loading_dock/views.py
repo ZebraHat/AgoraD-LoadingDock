293e6c32bfb15051d2ef22c13a31e0ef6ec09fff
Ubuntu <ubuntu@ip-10-171-13-241.ec2.internal>
2013-12-03 22:09:16 +0000

    uuuu

--- AgoraD/loading_dock/views.py
+++ AgoraD/loading_dock/views.py
@@ -1,6 +1,14 @@
 from django.http import HttpResponse
 from django.core import serializers
 from ModelGenerator import getModel, getSQL
+from django.views.decorators.csrf import csrf_exempt
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+import urllib2
+
+
 import models
 import JsonSerializer
 import json
@@ -41,7 +49,8 @@ def listdbs(request):
     return response
 
 def newschema(request):
-    json = '{"data": {"itworks": {"a1": "(\'IntegerField\', {\'primary_key\': True})", "a2": "(\'TextField\', {})"}}}'
+#    json = '{"data": {"itworks": {"a1": "(\'IntegerField\', {\'primary_key\': True})", "a2": "(\'TextField\', {})"}}}'
+    json = '{"students": {"information": {"id": "(\'IntegerField\', {\'primary_key\': True})", "name": "(\'TextField\', {})", "major": "(\'TextField\', {})"}}}'
 
     JsonSerializer.json2schema(json)
     response = HttpResponse()
@@ -50,16 +59,25 @@ def newschema(request):
 
 def newdata(request):
     #json = r'[{"fields": {"c2": "hello", "c1": 1}, "class": "newtable"}, {"fields": {"c2": "asdf", "c1": 5}, "class": "newtable"}]'
-    json = r'[{"fields": {"a2": "hello", "a1": 1}, "class": "itworks"}, {"fields": {"a2": "asdf", "a1": 5}, "class": "itworks"}]'
+    #json = r'[{"fields": {"a2": "hello", "a1": 1}, "class": "itworks"}, {"fields": {"a2": "asdf", "a1": 5}, "class": "itworks"}]'
+    json = '[{"fields": {"id": 12345, "name": "jeff", "major":"CS"}, "class": "information"},{"fields": {"id": 54321, "name": "sarah", "major":"CpE"}, "class": "information"},{"fields": {"id": 666, "name": "leo", "major":"IST"}, "class": "information"}]'
 
     response = HttpResponse()
-    for obj in JsonSerializer.deserialize(json, 'data'):
-        response.write((obj.a1, obj.a2))
+    for obj in JsonSerializer.deserialize(json, 'students'):
         obj.save()
 
 #    json = r'[{"pk": 3, "model": "loading_dock.ModelGenerator.data.newtable", "fields": {"c2": "hello"}}, {"pk": 4, "model": "loading_dock.ModelGenerator.data.newtable", "fields": {"c2": "asdf"}}]'
 
 #    for obj in serializers.deserialize('json', json):
 #        print obj
+    return response
+
+def adddata(request):
+    pass
+
+@api_view(['POST'])
+def addschema(request):
+    response = HttpResponse()
+    response.write (str(request.QUERY_PARAMS.dict()))
     return response
 
