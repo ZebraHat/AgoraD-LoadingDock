aa6256c2832a23ad4bcb77459090bcdc0586a420
LinuxMercedes <LinuxMercedes@gmail.com>
2013-12-05 14:43:23 -0600

    Fix highway url handler

--- AgoraD/highway/urls.py
+++ AgoraD/highway/urls.py
@@ -1,8 +1,7 @@
 from django.conf.urls import patterns, include, url
 
 
-urlpatterns = patterns('',
-    'highway.views',
+urlpatterns = patterns( 'highway.views',
     url(r'^transfer/start/$', 'transfer_start'),
     url(r'^transfer/schema/$', 'transfer_schema'),
     url(r'^transfer/block/', 'transfer_block'),
