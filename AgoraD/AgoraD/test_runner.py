2d1c8d251910308cf55698faedebf0acff8254dc
Chase Holland <chase.holland@lumate.com>
2013-12-06 14:26:33 -0600

    adding the test runner...

---
+++ AgoraD/AgoraD/test_runner.py
@@ -0,0 +1,32 @@
+#
+## test_runner.py
+## custom test runner that bypasses the creation of a database on this side
+#
+
+from django.test.simple import DjangoTestSuiteRunner
+from loading_dock.management.commands.add_db import Command
+
+
+class LoadFixturesRunner(DjangoTestSuiteRunner):
+    """ A test runner to test without database creation """
+    def run_tests(self, test_labels, extra_tests=None, **kwargs):
+
+        self.setup_test_environment()
+        suite = self.build_suite(test_labels, extra_tests)
+        old_config = self.setup_databases()
+
+        print 'Running script'
+        ### Run the add db command ###
+
+        c = Command()
+        c.handle('test_app_postgresql')
+        c.handle('test_app_sqlite')
+
+        ###                        ###
+
+        result = self.run_suite(suite)
+
+        self.teardown_databases(old_config)
+        self.teardown_test_environment()
+
+        return self.suite_result(suite, result)
